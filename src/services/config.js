5dea23b8b1898f4ef6f11dfdac572700416d7821
cortezemanuel <109178075+cortezemanuel@users.noreply.github.com>
2025-02-20 19:28:13 -0300

    proyecto final

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY 
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY, 
   authDomain: "xmas-cottage.firebaseapp.com",
   projectId: "xmas-cottage",
   storageBucket: "xmas-cottage.appspot.com",
